de807811749c1a08a3d832196051be8c6d201d47
Rashid Ameer <r.ameer.offical@gmail.com>
2026-04-24 18:13:20 +0500

    Updated health response for testing

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/health", (_, res) => {
-  res.status(200).json({ message: "Health is 100%" });
+  res.status(200).json({ status: "ok", message: "updated version" });
 });
 
 export default app;
